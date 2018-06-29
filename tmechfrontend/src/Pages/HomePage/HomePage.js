934009487fa26822de438112a447fea44d381c0f
Halil Ozan Akgül <hozanakgul@gmail.com>
2018-06-29 23:21:20 +0200

    Style
    Results are styled

--- tmechfrontend/src/Pages/HomePage/HomePage.js
+++ tmechfrontend/src/Pages/HomePage/HomePage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Dropdown, Form, Header, Container, Input, Segment, Grid, Button} from "semantic-ui-react";
+import {Image, Dropdown, Form, Header, Container, Input, Segment, Grid, Button} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 import axios from "axios";
+import "./HomePage.css";
 
 export default class HomePage extends React.Component {
   constructor(props){
@@ -24,7 +25,7 @@ export default class HomePage extends React.Component {
           album: "",
           spotify_url: "",
           spotify_id: "",
-          album_photo: "",
+          album_image: "",
         }
       ],
     }
@@ -47,6 +48,7 @@ export default class HomePage extends React.Component {
       this.setState({
         search_result: response.data
       })
+      console.log(this.state.search_result);
     })
   }
 
@@ -63,9 +65,32 @@ export default class HomePage extends React.Component {
                   <input value={this.state.search_text} onChange={this.handleChangeSearch_text}/>
                 </Form.Field>
               </Form>
-              {
+              { this.state.search_result[0]["title"].length>0 && 
                 this.state.search_result.map((song) =>(
-                  <div>{song.title}</div>
+                  <Segment className="search_result">
+                    <Grid>
+                      <Grid.Row style = {{"padding":"0"}}>
+                        <Grid.Column width = {3} style = {{"padding-left":"0"}}>
+                          <Image src={song.album_image} />
+                        </Grid.Column>
+                        <Grid.Column width = {13}>
+                          <Grid>
+                            <Grid.Row style = {{"padding-bottom":"0", "padding-top":"32px"}}>
+                              <div style = {{"font-size":"18pt"}}>{song.title}</div>
+                            </Grid.Row>
+                            <Grid.Row>
+                              <Grid.Column width = {6} style = {{"padding-left":"0"}}>
+                                <div style={{"color":"grey"}}>Artist:{song.artist}</div>
+                              </Grid.Column>
+                              <Grid.Column width = {10}>
+                                <div style={{"color":"grey"}}>Album:{song.album}</div>
+                              </Grid.Column>
+                            </Grid.Row>
+                          </Grid>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                  </Segment>
                 ))
               }
             </Grid.Column>
